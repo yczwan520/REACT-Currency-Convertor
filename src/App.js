9e4219972b5c3df10ef8fe3d365547d8a997205f
yczwan520 <111413630+yczwan520@users.noreply.github.com>
2022-11-04 03:10:36 -0400

    change css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React, { useState } from "react";
-import { DatePicker, Space } from "antd";
-import "./App.css";
-import moment from "moment";
-import ExchangeCurrency from "./ExchangeCurrency";
+import React from "react";
+import ExchangeCurrency from "./components/CurrencyRates/ExchangeCurrency";
 
 const App = () => {
-  const [date, setDate] = useState("");
-
-  const handleDateChange = (date) => {
-    setDate(moment(date).format("YYYY-MM-DD"));
-  };
-
   return (
     <div>
-      <h1>Currency Convertor</h1>
-      <p className="date">date</p>
-      <DatePicker
-        onChange={(date) => handleDateChange(date)}
-      />
-      <ExchangeCurrency date={date} />
+      <ExchangeCurrency />
     </div>
   );
 };
